56fa09e8b1498542b43e90664b465489bd985b6e
Edem <edem.quashigah@student.hamk.fi>
2025-01-13 08:21:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# randomideas-app
+ A fullstack application for sharing random ideas. A project from Brad Traversy's "Modern JS From The Beginning" course.
---
+++ routes/ideas.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const router = express.Router();
+const Idea = require('../models/Idea');
+
+
+// const ideas = [
+//     {
+//         id:  1,
+//         text: 'Positive NewLetter, a newsletter that only shares positive, uplifting news',
+//         tag: 'Technology',
+//         username: 'TonyStark',
+//         date: '2022-05-01',
+//     },
+//     {
+//         id:  2,
+//         text: 'Milk cartons that turn a different color the older that your milk is getting',
+//         tag: 'Inventions',
+//         username: 'SteveRogers',
+//         date: '2021-05-02',
+//     },
+//     {
+//         id:  3,
+//         text: 'ATM locatin app which lets yuo know where the closest ATM is and if it is in service',
+//         tag: 'Software',
+//         username: 'BruceBanner',
+//         date: '2021-05-02',
+//     },
+// ];
+
+// Get all ideas
+router.get('/', async (req, res) => {
+    try {
+        const ideas = await Idea.find();
+        res.json(({success: true, data: ideas}));
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, error: 'Something went wrong'});
+    }
+})
+
+// Get single idea
+router.get('/:id', async (req, res) => {
+    try {
+        const idea = await Idea.findById(req.params.id);
+        res.json(({success: true, data: idea}));
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, error: 'Something went wrong'});
+    }
+})
+
+// Add an idea
+router.post('/', async (req, res) => {
+
+    const idea = new Idea({
+        text: req.body.text,
+        tag: req.body.tag,
+        username: req.body.username
+    });
+
+    try {
+        const savedIdea = await idea.save();
+        res.json({success: true, data: idea});
+        
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, error: 'Something went wrong'});
+    }
+})
+
+// Update an idea
+router.put('/:id', async (req, res) => {
+
+    try {
+        const updatedIdea = await Idea.findByIdAndUpdate(
+            req.params.id, 
+            {
+                $set: {
+                    text: req.body.text,
+                    tag: req.body.tag
+                }
+            }, 
+            {new: true} 
+        );
+        res.json({success: true, data: updatedIdea});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, error: 'Something went wrong'});
+    }
+})
+
+// Delete an idea
+router.delete('/:id', async (req, res) => {
+    try {
+        await Idea.findByIdAndDelete(req.params.id);
+        res.json({success: true, data: {} });
+    } catch (error) {
+        console.log(error),
+        res.status(500).json({ success: false, error: 'Something went wrong'}); 
+    }
+})
+
+
+
+module.exports = router;    // Export the router object so that it can be used in other files.
