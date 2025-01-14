1b7792af6906082722932d6beb3b70ccdc101588
Edem <edem.quashigah@student.hamk.fi>
2025-01-14 08:05:55 +0200

    Modal.js component created
    Fixed index.html Idea form modal boilerplate.

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,26 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './css/style.css';
+
+import Modal from './components/Modal';
+
+const modal = new Modal();
+
+// const modal = document.querySelector('#modal'); 
+// const modalBtn = document.querySelector('#modal-btn'); 
+
+// function open() {
+//     modal.style.display = "block";
+// }
+
+// function close() {
+//     modal.style.display= "none";
+// }
+
+// function outsideClick(e) {
+//     if(e.target == modal){
+//         close();
+//     }
+// }
+
+// modalBtn.addEventListener('click',open);
+// window.addEventListener('click', outsideClick); 
