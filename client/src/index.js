03a4502969ad73bfd25ea0178695f5cd74903f39
Edem <edem.quashigah@student.hamk.fi>
2025-01-14 09:15:25 +0200

    IdeaForm and IdeaList components created

--- client/src/index.js
+++ client/src/index.js
@@ -2,25 +2,12 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import './css/style.css';
 
 import Modal from './components/Modal';
+import IdeaForm from './components/IdeaForm';
+import IdeaList from './components/IdeaList';
 
 const modal = new Modal();
+const ideaForm = new IdeaForm();
+const ideaList = new IdeaList();
 
-// const modal = document.querySelector('#modal'); 
-// const modalBtn = document.querySelector('#modal-btn'); 
-
-// function open() {
-//     modal.style.display = "block";
-// }
-
-// function close() {
-//     modal.style.display= "none";
-// }
-
-// function outsideClick(e) {
-//     if(e.target == modal){
-//         close();
-//     }
-// }
-
-// modalBtn.addEventListener('click',open);
-// window.addEventListener('click', outsideClick); 
+ideaForm.render();
+ideaList.render();
